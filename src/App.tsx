d53f61fd891c6356e868fb15d50d0cf7002f2e18
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-07-07 15:23:18 +0100

    import the faq component and each one has his own information

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import { useState } from "react";
-import { FaMinusCircle, FaPlusCircle, FaStar } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
+import Faq from "./Faq";
 
 function App() {
-  const [show, setShow] = useState(false);
 
   return (
     <>
@@ -12,91 +11,46 @@ function App() {
           <FaStar className="text-purple-500" />{" "}
           <span className="pl-5">FAQs</span>
         </h1>
-        <div className="hover:shadow-md p-3 rounded-md">
-          <div className="flex text-purple-950 justify-between">
-            <h2 className="text-md font-bold flex-wrap">
-              What is frontend mentor, and how it will help you
-            </h2>
-            <button>
-              {show ? (
-                <FaPlusCircle onClick={() => setShow(!show)} />
-              ) : (
-                <FaMinusCircle onClick={() => setShow(!show)} />
-              )}
-            </button>
-          </div>
-          {!show && (
-            <p className="my-3 opacity-60">
-              Frontend Mentor offers realisitic coding challenges to help
-              developers improve their frontend coding skills with projects in
-              HTML, CSS and javascript. it's suitable for all levels and ideal
-              for portfolio building
-            </p>
-          )}
-        </div>
-        <div className="hover:shadow-md p-3 rounded-md">
-          <div className="flex text-purple-950 justify-between">
-            <h2 className="text-md font-bold flex-wrap">
-              Is Frontend Mentor free ?
-            </h2>
-            <button>
-              {show ? (
-                <FaPlusCircle onClick={() => setShow(!show)} />
-              ) : (
-                <FaMinusCircle onClick={() => setShow(!show)} />
-              )}
-            </button>
-          </div>
-          {!show && (
-            <p className="my-3 opacity-60">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Molestiae nostrum doloribus fuga beatae assumenda adipisci
-              accusantium laborum odit animi consequatur.
-            </p>
-          )}
-        </div>
-        <div className="hover:shadow-md p-3 rounded-md">
-          <div className="flex text-purple-950 justify-between">
-            <h2 className="text-md font-bold flex-wrap">
-              Can i use Frontend Mentor projects in my portfolio ?
-            </h2>
-            <button>
-              {show ? (
-                <FaPlusCircle onClick={() => setShow(!show)} />
-              ) : (
-                <FaMinusCircle onClick={() => setShow(!show)} />
-              )}
-            </button>
-          </div>
-          {!show && (
-            <p className="my-3 opacity-60">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Blanditiis iure distinctio incidunt eveniet minima harum.
-            </p>
-          )}
-        </div>
-        <div className="hover:shadow-md p-3 rounded-md">
-          <div className="flex text-purple-950 justify-between">
-            <h2 className="text-md font-bold flex-wrap">
-              How can i get help if i'm stuck on a challenges
-            </h2>
-            <button>
-              {show ? (
-                <FaPlusCircle onClick={() => setShow(!show)} />
-              ) : (
-                <FaMinusCircle onClick={() => setShow(!show)} />
-              )}
-            </button>
-          </div>
-          {!show && (
-            <p className="my-3 opacity-60">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore ab
-              architecto vero nesciunt blanditiis, molestias officia suscipit
-              delectus nostrum facilis assumenda aperiam sint. Facere,
-              molestias.
-            </p>
-          )}
-        </div>
+        <Faq
+          key="faq-1"
+          question={"What is frontend mentor, and how it will help you"}
+          answer={
+            "Frontend Mentor offers realisitic coding challenges to help " +
+            " developers improve their frontend coding skills with projects in" +
+            "HTML, CSS and javascript. it's suitable for all levels and ideal" +
+            "for portfolio building"
+          }
+        />
+
+        <Faq
+          key="faq-2"
+          question={"Is Frontend Mentor free ?"}
+          answer={
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit." +
+            "Molestiae nostrum doloribus fuga beatae assumenda adipisci" +
+            "accusantium laborum odit animi consequatur."
+          }
+        />
+
+        <Faq
+          key="faq-3"
+          question={"Can i use Frontend Mentor projects in my portfolio ?"}
+          answer={
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. " +
+            "Blanditiis iure distinctio incidunt eveniet minima harum."
+          }
+        />
+
+        <Faq
+          key="faq-4"
+          question={"How can i get help if i'm stuck on a challenges?"}
+          answer={
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore ab" +
+            "architecto vero nesciunt blanditiis, molestias officia suscipit" +
+            "delectus nostrum facilis assumenda aperiam sint. Facere," +
+            "molestias."
+          }
+        />
       </div>
     </>
   );
